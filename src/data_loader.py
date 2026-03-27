1beb0cad69b549de56477cc46b10a909477de385
yhamzaoui-ensae <youssef.hamzaoui@ensae.fr>
2026-03-27 09:21:47 +0000

    Chargement des données

---
+++ src/data_loader.py
@@ -0,0 +1,24 @@
+import os
+import s3fs
+import pandas as pd
+
+def load_data() -> pd.DataFrame:
+    """
+    Fonction permettant de charger les données au format .xlxs
+    depuis Onyxia et de les convertir en un DataFrame
+
+    """
+    endpoint = os.environ["AWS_S3_ENDPOINT"]
+    if not endpoint.startswith('http'):
+        endpoint = "https://" + endpoint
+    
+    fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url" : endpoint})
+
+    path = ("projet-bdc-data/apicil/"
+            "Copie de V5_ADENA_extract client indiv direct et courté.xlsx"
+    )
+
+    with fs.open(path, "rb") as f:
+        df = pd.read_excel(f)
+
+    return df
